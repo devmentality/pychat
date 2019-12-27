cbc2450de3ac92e26f7679a4d2772ac2705d73b6
devmentality <nikita.volnov.1999@mail.ru>
2019-12-27 16:59:23 +0500

    Basic api, id authentication

---
+++ static/app.js
@@ -0,0 +1,13 @@
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+const userId = getCookie('userId');
+
+if (userId === undefined)
+    document.location.replace('/login');
+else
+    alert('Welcome!');
